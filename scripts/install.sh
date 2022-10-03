2efbebba2f780d6afcfb070b5edebf6c6346760d
Cristiano Vitorino <57003922+cristianovitorino@users.noreply.github.com>
2022-10-03 10:53:09 -0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
-# Fedora
-
+# Ubuntu
 echo "Installing required software..."
-
 sleep 2
 
 # Base
-sudo dnf install -y google-noto-sans-fonts adobe-source-sans-pro-fonts polybar rofi nautilus-image-converter seahorse-nautilus sushi p7zip maim lxappearance picom network-manager-applet imwheel dmenu dunst nitrogen pavucontrol feh terminator w3m adwaita-qt flameshot solaar kvantum
+sudo apt install -y i3 font-manager polybar rofi nautilus-image-converter seahorse-nautilus nautilus-script-manager nautilus-scripts-manager nautilus-admin nautilus-font-manager nautilus-extension-gnome-console nautilus-extension-gnome-terminal gnome-sushi p7zip p7zip-full maim lxappearance picom nm-tray polkitd policykit-desktop-privileges imwheel suckless-tools dunst nitrogen pasystray pavucontrol feh terminator w3m adwaita-icon-theme-full adwaita-qt flameshot solaar qt5-style-kvantum qt5-style-kvantum-themes
 
 # Apply Kvantum for QT applications
 touch ~/.profile && echo "export QT_STYLE_OVERRIDE=kvantum" >> ~/.profile
-
-# i3-Gaps
-sudo dnf -y copr enable fuhrmann/i3-gaps
-sudo dnf install -y i3-gaps
-
-# Font Manager
-sudo dnf -y copr enable jerrycasiano/FontManager
-sudo dnf install -y font-manager
 
 sleep 2
 
