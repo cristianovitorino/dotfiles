cef6c320ad0aebdc19eec7992f3b251ab74cc736
Cristiano Vitorino <57003922+cristianovitorino@users.noreply.github.com>
2022-10-03 14:01:50 -0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ echo "Installing required software..."
 sleep 2
 
 # Base
-sudo apt install -y i3 font-manager polybar rofi nautilus-image-converter seahorse-nautilus nautilus-script-manager nautilus-scripts-manager nautilus-admin nautilus-font-manager nautilus-extension-gnome-console gnome-terminal nautilus-extension-gnome-terminal gnome-sushi p7zip p7zip-full maim lxappearance picom nm-tray polkitd policykit-desktop-privileges imwheel suckless-tools dunst nitrogen pasystray pavucontrol feh terminator w3m adwaita-icon-theme-full adwaita-qt flameshot solaar qt5-style-kvantum qt5-style-kvantum-themes
+sudo apt install -y font-manager polybar rofi nautilus-image-converter seahorse-nautilus nautilus-script-manager nautilus-scripts-manager nautilus-admin nautilus-font-manager nautilus-extension-gnome-console gnome-terminal nautilus-extension-gnome-terminal gnome-sushi p7zip p7zip-full maim lxappearance picom nm-tray polkitd policykit-desktop-privileges imwheel suckless-tools dunst nitrogen pasystray pavucontrol feh terminator w3m adwaita-icon-theme-full adwaita-qt flameshot solaar qt5-style-kvantum qt5-style-kvantum-themes
 
 # Apply Kvantum for QT applications
 touch ~/.profile && echo "export QT_STYLE_OVERRIDE=kvantum" >> ~/.profile
